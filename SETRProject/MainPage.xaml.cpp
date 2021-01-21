c15430f74c3f212e64b3f7adca14466d74bd9c2a
Thomas BAGREL <tomsb07@gmail.com>
2021-01-21 18:24:15 +0100

    http request

--- SETRProject/MainPage.xaml.cpp
+++ SETRProject/MainPage.xaml.cpp
@@ -1,20 +1,24 @@
-﻿//
+﻿#include "pch.h"
+
+//
 // MainPage.xaml.cpp
 // Implémentation de la classe MainPage.
 //
 
 #include <ctime>
+#include <codecvt>
+#include <locale>
+#include <string>
 
-#include "pch.h"
 #include "MainPage.xaml.h"
 #include "FindNearestResult.h"
 #include "Mutexed.h"
 #include "ReadSensorsResult.h"
 #include "UserPositionResult.h"
 #include "TimeUtils.h"
+#include "json.hpp"
 
 using namespace SETRProject;
-
 using namespace Platform;
 using namespace Windows::Foundation;
 using namespace Windows::Foundation::Collections;
@@ -25,6 +29,7 @@ using namespace Windows::UI::Xaml::Data;
 using namespace Windows::UI::Xaml::Input;
 using namespace Windows::UI::Xaml::Media;
 using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Web::Http;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -38,10 +43,59 @@ Mutexed<FindNearestResult> findNearestResult;
 Mutexed<bool> findNearestRunning(true);
 Mutexed<UserPositionResult> userPositionResult;
 
+std::wstring toWstring(std::string source) {
+	std::wstring_convert<std::codecvt_utf8_utf16<wchar_t>> converter;
+	return converter.from_bytes(source);
+}
+
+const std::wstring apiUrl = L"http://iotlab.telecomnancy.eu:8080/iotlab/rest/data/1/temperature/last";
+HttpClient httpClient;
+
+std::vector<TempPoint> parseJsonData(std::wstring const& jsonString) {
+	std::vector<TempPoint> result;
+	nlohmann::json moteList = nlohmann::json::parse(jsonString);
+	for (auto &moteObj : moteList) {
+		result.push_back(TempPoint::fromJson(moteObj));
+	}
+	return result;
+}
+
+ReadSensorsResult readSensors(std::wstring apiUrl) {
+	Platform::String^ apiUrlPlatform = ref new Platform::String(apiUrl.c_str());
+	Uri^ requestUri = ref new Uri(apiUrlPlatform);
+	std::vector<TempPoint> points;
+	std::wstring errorMessage = L"";
+	boolean isSuccess = true;
+	try {
+		HttpResponseMessage^ response = concurrency::create_task(httpClient.GetAsync(requestUri)).get();
+		try {
+			Platform::String^ responseBody = concurrency::create_task(response->Content->ReadAsStringAsync()).get();
+			try {
+				points = parseJsonData(responseBody->Data());
+			}
+			catch (nlohmann::json::exception& ex) {
+				isSuccess = false;
+				errorMessage = toWstring(ex.what());
+				// Json exception
+			}
+		}
+		catch (Platform::Exception ^ex) {
+			isSuccess = false;
+			errorMessage = std::wstring(ex->Message->Data());
+			// Read body exception
+		}
+	}
+	catch (Platform::Exception ^ex) {
+		isSuccess = false;
+		errorMessage = std::wstring(ex->Message->Data());
+		// HTTP call exception
+	}
+	return ReadSensorsResult(timestampNow(), isSuccess, errorMessage, points);
+}
+
 void readSensorsThreadFunc() {
 	while (readSensorsRunning.read()) {
-		// TODO: read sensors
-		readSensorsResult.update([](ReadSensorsResult prevResult) { return ReadSensorsResult(timestampNow(), true, L"", std::vector<TempPoint>()); });
+		readSensorsResult.update([](ReadSensorsResult prevResult) { return readSensors(apiUrl); });
 		std::this_thread::sleep_for(std::chrono::milliseconds(READ_SENSORS_SLEEP_MILLISECONDS));
 	}
 }
--- SETRProject/TempPoint.h
+++ SETRProject/TempPoint.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <string>
+#include "json.hpp"
 
 struct TempPoint {
 	uint64_t instant;
@@ -10,4 +11,12 @@ struct TempPoint {
 	TempPoint() : instant(0), moteId(L""), value(0) {}
 	TempPoint(const TempPoint& that) : instant(that.instant), moteId(that.moteId), value(that.value) {}
 	TempPoint(uint64_t _instant, std::wstring _moteId, double _value) : instant(_instant), moteId(_moteId), value(_value) {}
+
+	static TempPoint fromJson(nlohmann::json const& obj) {
+		return TempPoint(
+			obj.at("timestamp"),
+			obj.at("mote"),
+			obj.at("value")
+		);
+	}
 };
